780af64748e05d7acab1355e58f65445a360ba50
ibocus@hotmail.com <ibocus@intercontinentaltrust.com>
2020-05-19 12:26:01 +0400

    mdy

--- Sum.R
+++ Sum.R
@@ -1,1 +1,7 @@
-sum(1,3)
+library(tidyverse)
+nuclear_accidents <- read_csv("https://query.data.world/s/47s7katrhxxd674ulus425k42l5u4s") 
+nuclear_accidents <- nuclear_accidents %>% 
+  select(-Description) %>% 
+  mutate(Year = Date %>% mdy() %>% year(), In_Europe = if_else(Region %in% c("EE", "WE"), T, F) %>% as.factor()) %>% 
+  filter(Year %>% between(1989, 2016))
+
